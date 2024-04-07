f3fc4b0655879c016edf30372ebd1115427cfcb2
starboticaia <165588679+starboticaia@users.noreply.github.com>
2024-04-07 19:00:04 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 
-if st.button("Hola"):
-  st.write("Hola")
-else:
-  st.write("Adiós")
+# demo de texto y botón
+animales = ['gato', 'perro', 'pez', 'lagarto']
+
+animal = st.text_input('Pide un animal...')
+
+if st.button('Ver disponibilidad'):
+    disponible = animal.lower() in animales
+    if disponible:
+      st.write('¡Lo tenemos!')
+    else:
+      st.write('Lo sentimos, no lo tenemos.')
