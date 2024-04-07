513210db35540938c6ed9f0e47c348d3c72ea2b7
starboticaia <165588679+starboticaia@users.noreply.github.com>
2024-04-07 20:27:32 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,4 @@ pulgadas = st.number_input('Pulgadas:')
 if st.button('Calcular'):
     centimetros = pulgadas*2.51
     # añadir :.2f a un número para limitarlo a dos decimales
-    st.write(f'{pulgadas}" equivalen a {centimetros}:.2f cm.')
+    st.write(f'{pulgadas}" equivalen a {centimetros:.2f} cm.')
